3e690b912eaf41ac8eabb39eb5b491367c6ff25e
KadeCode15 <49326307+KadeCode15@users.noreply.github.com>
2020-02-15 18:07:36 -0500

    Updated fingering chart to withstand out of range exceptions

--- untitled/src/target.py
+++ untitled/src/target.py
@@ -1,6 +1,20 @@
 import midi
 
-pattern = midi.read_midifile("369646.mid")
-print(pattern)
+pattern = midi.read_midifile("../369646.mid")
 
-print("Hello")
+
+
+tracks = []
+
+trackCounter = 0
+for track in pattern:
+    tracks.append(track)
+
+    if trackCounter == 5:
+        print(tracks[trackCounter])
+
+    trackCounter = trackCounter + 1
+
+
+if isinstance(tracks[0][0], midi.events.TrackNameEvent):
+    print("It is the same")
