ec2af183b6d5d70d3c54c9114ac9975fd62d5566
Aryan Wadhwani <wadhwani@purdue.edu>
2020-02-15 16:11:19 -0600

    hello

--- untitled/src/target.py
+++ untitled/src/target.py
@@ -1,6 +1,15 @@
+import sys
+sys.path.insert(0, "../lib")
 import midi
+from lib import Leap
 
 pattern = midi.read_midifile("369646.mid")
 print(pattern)
+
+controller = Leap.Controller()
+controller.set_policy(Leap.Controller.POLICY_BACKGROUND_FRAMES)
+controller.set_policy(Leap.Controller.POLICY_IMAGES)
+controller.set_policy(Leap.Controller.POLICY_OPTIMIZE_HMD)
+
 
 print("Hello")
