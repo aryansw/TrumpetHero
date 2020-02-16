1058ae708d43d757813764aebc2a3f611e17beba
Aryan Wadhwani <wadhwani@purdue.edu>
2020-02-15 18:05:46 -0600

    Seperated MIDI into trumpet and backing tracks
    wdw

--- untitled/src/target.py
+++ untitled/src/target.py
@@ -1,32 +1,26 @@
-import sys
-sys.path.insert(0, "../lib")
 import midi
 import pygame
 
 pygame.init()
 
-pattern = midi.read_midifile("../369646.mid")
+pattern = midi.read_midifile("music/bohemian.mid")
 
 
 # temporary code
-tracks = []
+tracks = midi.Pattern()
 
 trackCounter = 0
 for track in pattern:
-    tracks.append(track)
-
-    if trackCounter == 5:  # change this number to find different instruments/vocals
-        print(tracks[trackCounter])
-
+    if trackCounter != 3:
+        tracks.append(track)
     trackCounter = trackCounter + 1
 
-
-if isinstance(tracks[0][0], midi.events.TrackNameEvent):
-    print("It is the same")
-
 # end of temporary code
+trumpet = pattern[3]
+pattern.remove(trumpet)
+midi.write_midifile("backingsong.mid", pattern)
 
-pygame.mixer.music.load("369646.mid")
+pygame.mixer.music.load("backingsong.mid")
 pygame.mixer.music.play()
 
 while pygame.mixer.music.get_busy():
