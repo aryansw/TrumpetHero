610f8bb898b70d35a475cbc2642406014b4133e7
KadeCode15 <49326307+KadeCode15@users.noreply.github.com>
2020-02-15 19:01:33 -0500

    Changed target.py file to accomodate new song

--- untitled/src/target.py
+++ untitled/src/target.py
@@ -1,10 +1,10 @@
 import sys
+
 sys.path.insert(0, "../lib")
 import midi
 from lib import Leap
 
-pattern = midi.read_midifile("../369646.mid")
-
+pattern = midi.read_midifile("../BohemianRhapsody.mid")
 
 # temporary code
 tracks = []
@@ -14,13 +14,21 @@ for track in pattern:
     tracks.append(track)
 
     if trackCounter == 5:  # change this number to find different instruments/vocals
-        print(tracks[trackCounter])
+        i = 0
+        # print(tracks[trackCounter])
 
     trackCounter = trackCounter + 1
 
-
 if isinstance(tracks[0][0], midi.events.TrackNameEvent):
     print("It is the same")
+
+
+trackNum = 3
+for sub in tracks[trackNum]:
+    if isinstance(sub, midi.events.NoteOnEvent) or isinstance(sub, midi.events.TrackNameEvent) or isinstance(sub, midi.events.TextMetaEvent):
+        print(sub)
+        print(sub.tick)
+
 
 # end of temporary code
 
@@ -28,6 +36,5 @@ controller = Leap.Controller()
 controller.set_policy(Leap.Controller.POLICY_BACKGROUND_FRAMES)
 controller.set_policy(Leap.Controller.POLICY_IMAGES)
 controller.set_policy(Leap.Controller.POLICY_OPTIMIZE_HMD)
-
 
 print("Hello")
