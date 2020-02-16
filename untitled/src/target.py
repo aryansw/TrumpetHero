7f0d22260a38fc59aad72a46a9d8f09b6f84d4fe
KadeCode15 <49326307+KadeCode15@users.noreply.github.com>
2020-02-15 20:17:49 -0500

    ticks now are absolute based rather than relative

--- untitled/src/target.py
+++ untitled/src/target.py
@@ -1,12 +1,16 @@
 import sys
 
+from lib import Leap
+
 sys.path.insert(0, "../lib")
 import midi
 import pygame
 
 pygame.init()
 
-pattern = midi.read_midifile("../BohemianRhapsody.mid")
+# pattern = midi.read_midifile("../BohemianRhapsody.mid")
+pattern = midi.read_midifile("369646.mid")
+pattern.make_ticks_abs()
 
 # temporary code
 tracks = []
@@ -24,12 +28,17 @@ for track in pattern:
 if isinstance(tracks[0][0], midi.events.TrackNameEvent):
     print("It is the same")
 
+prev_sub_tick = 0
 
-trackNum = 3
+trackNum = 5
 for sub in tracks[trackNum]:
     if isinstance(sub, midi.events.NoteOnEvent) or isinstance(sub, midi.events.TrackNameEvent) or isinstance(sub, midi.events.TextMetaEvent):
-        print(sub)
-        print(sub.tick)
+        if sub.tick > 0:
+            dif = sub.tick - prev_sub_tick
+            prev_sub_tick = sub.tick
+            if dif > 0:
+                print(sub)
+                print(dif)
 
 
 # end of temporary code
@@ -41,8 +50,8 @@ controller.set_policy(Leap.Controller.POLICY_IMAGES)
 controller.set_policy(Leap.Controller.POLICY_OPTIMIZE_HMD)
 
 
-#pygame.mixer.music.load("369646.mid")
-#pygame.mixer.music.play()
+# pygame.mixer.music.load("369646.mid")
+# pygame.mixer.music.play()
 
-#while pygame.mixer.music.get_busy():
+# while pygame.mixer.music.get_busy():
 #    pygame.time.wait(1000)
