0fa4e9b986abaac3eb74e88a69c529057e98c897
KadeCode15 <49326307+KadeCode15@users.noreply.github.com>
2020-02-15 22:46:04 -0500

    some changes

--- untitled/src/target.py
+++ untitled/src/target.py
@@ -1,17 +1,22 @@
 import sys
 from lib import Leap
 
+from midi2audio import FluidSynth
+
 sys.path.insert(0, "../lib")
 import midi
 import pygame
 
 pygame.init()
 
+
 pattern = midi.read_midifile("music/bohemian.mid")
 pattern.make_ticks_abs()
 
+
+
 # temporary code
-tracks = midi.Pattern()
+tracks = pattern
 
 #trackCounter = 0
 #trackNum = 0
@@ -35,15 +40,20 @@ tracks = midi.Pattern()
 
 prev_sub_tick = 0
 
-trackNum = 5
+trackNum = 0
 for sub in tracks[trackNum]:
-    if isinstance(sub, midi.events.NoteOnEvent) or isinstance(sub, midi.events.TrackNameEvent) or isinstance(sub, midi.events.TextMetaEvent):
-        if sub.tick > 0:
-            dif = sub.tick - prev_sub_tick
-            prev_sub_tick = sub.tick
-            if dif > 0:
-                print(sub)
-                print(dif)
+    # if isinstance(sub, midi.events.NoteOnEvent) or isinstance(sub, midi.events.TrackNameEvent) or isinstance(sub, midi.events.TextMetaEvent) or isinstance(sub, midi.events.SetTempoEvent):
+      #   if sub.tick > 0:
+          #  dif = sub.tick - prev_sub_tick
+         #   prev_sub_tick = sub.tick
+        #    if dif > 0:
+    if isinstance(sub, midi.events.SetTempoEvent):
+        print(sub)
+        print(60000 / ((sub.get_mpqn() / 1000) * sub.get_bpm()))
+        print(sub.get_mpqn())
+        print(sub.get_bpm())
+        print('\n')
+         #       print(dif)
 
 
 # end of temporary code
@@ -65,7 +75,16 @@ trumpet = pattern[trackNum]
 pattern.remove(trumpet)
 midi.write_midifile("backingsong.mid", pattern)
 
-pygame.mixer.music.load("backingsong.mid")
+#pygame.mixer.init()
+# Load two sounds
+#snd1 = pygame.mixer.Sound('music/369646.mid')
+#snd2 = pygame.mixer.Sound('music/bohemian.mid')
+# Play the sounds; these will play simultaneously
+#snd1.play()
+#snd2.play()
+
+pygame.mixer.music.load("music/369646.mid")
+pygame.mixer.music.load("music/bohemian.mid")
 pygame.mixer.music.play()
 
 while pygame.mixer.music.get_busy():
